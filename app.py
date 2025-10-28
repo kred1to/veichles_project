168d3604d86bbb687e5a1293a7347df87ac538dd
Daniel S. Gomes <danisgomes41@gmail.com>
2025-10-28 20:41:12 -0300

    Adiciona app Streamlit e configuração para render

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv(r'C:\Users\gomes\OneDrive\Documentos\repositórios\veichle project\veichles_project\vehicles.csv')
+
+st.header("Análise Exploratória de Dados - Conjunto de Dados de Anúncios de Vendas de Carros")
+
+st.write("Visualizando parte dos dados")
+st.dataframe(car_data.head())
+
+hist_button = st.button('Criar histograma')
+if hist_button:
+    st.write("Criando um histograma para o conjunto de dados de anúncios de vendas de carros")
+    fig = px.histogram(car_data, x="odometer", nbins=50, title="Distribuição do Odômetro dos Veículos Anunciados")
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_check = st.checkbox('Criar gráfico de dispersão (Preço x Odômetro)')
+
+if scatter_check:
+    st.write("Criando um gráfico de dispersão: relação entre Preço e Odômetro")
+    fig_scatter = px.scatter(car_data, x="odometer", y="price", title="Preço x Odômetro")
+    st.plotly_chart(fig_scatter, use_container_width=True)
